709a929aee3150ea4034bea042858814f38bf86e
Ryan Watts <rwatts3@users.noreply.github.com>
2018-11-28 10:34:51 -0700

    v0.9.1.3

--- kstack-kylo-dev/scripts/config.sh
+++ kstack-kylo-dev/scripts/config.sh
@@ -8,7 +8,7 @@ CLONE_URL=https://github.com/Teradata/kylo.git
 # Specify which branch you want to checkout, if cloning from a repository
 # Default is the default branch from the repository
 # eg: BRANCH=feature/new-awesome-feature
-BRANCH=release/0.8.1
+BRANCH=release/0.9.1.3
 
 # Specify which maven modules to copy Everything else is ignored
 # only supported: ui, services, integrations
